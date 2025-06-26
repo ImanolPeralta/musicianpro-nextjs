85ffa6207b68d0f3cef9f7cfedf99e623e14b3ba
ImanolPeralta <imanolaugusto18@gmail.com>
2025-06-26 19:50:05 -0300

    Updates

--- app/producto/[id]/page.js
+++
@@ -1,7 +0,0 @@
-import ProductDetail from '../../components/products/ProductDetail';
-
-export default function ProductoPage({ params }) {
-  const { id } = params;
-  console.log('ID recibido en ProductoPage:', id);
-  return <ProductDetail id={id} />;
-}
